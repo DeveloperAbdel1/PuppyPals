179357f1db570c120525c6d6126d715b7dc5dfb8
DeveloperAbdel <abdel.bakheit@gmail.com>
2023-08-05 22:05:51 -0400

    Render data with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { puppyList } from './data.js'
-import './App.css'
+import { useState } from "react";
+import { puppyList } from "./data";
+import "./App.css";
 
 function App() {
- 
-  console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  let featuredPup = puppies.find((pup) => pup.id === featPupId);
+
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p className ="Avatar" onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
